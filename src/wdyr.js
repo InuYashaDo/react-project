2eb079b14cbe36e149e15288a87832111584470a
guogang <986841218@qq.com>
2020-06-28 19:58:19 +0800

    Revert "first commit"
    This reverts commit a99b2b3a4f18e486857cb36a2b2a60098ee8f7de.

--- src/wdyr.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-if (process.env.NODE_ENV === 'development') { // 仅在本地开发中启用
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-  });
-}
